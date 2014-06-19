d2ab908f885416b0eda0b12b1f527e2eb82b5676
matthewfitz <matthewfitz@gmail.com>
2014-06-19 18:33:05 -0400

    Additional tests on FakeResponseClient

--- tests/AntiMattr/Tests/Sears/FakeResponseClientTest.php
+++ tests/AntiMattr/Tests/Sears/FakeResponseClientTest.php
@@ -66,6 +66,121 @@ class FakeResponseClientTest extends AntiMattrTestCase
 
         $this->client->findPurchaseOrdersByStatus('New');
     }
+
+    public function testCancelOrders()
+    {
+        $collection = $this->getMock('Doctrine\Common\Collections\Collection');        
+        $request = $this->buildMock('Buzz\Message\Form\FormRequest');
+        $response = $this->buildMock('Buzz\Message\Response');
+        $handler = $this->buildMock('AntiMattr\Sears\RequestHandler\AbstractRequestHandler');
+
+        $this->messageFactory->expects($this->once())
+            ->method('createRequest')
+            ->will($this->returnValue($request));
+
+        $this->requestHandlerFactory->expects($this->once())
+            ->method('createRequestHandler')
+            ->with('cancelOrders')
+            ->will($this->returnValue($handler));
+
+        $this->messageFactory->expects($this->once())
+            ->method('createResponse')
+            ->will($this->returnValue($response));
+
+        $this->client->cancelOrders($collection);
+    }
+
+    public function testReturnOrders()
+    {
+        $collection = $this->getMock('Doctrine\Common\Collections\Collection');        
+        $request = $this->buildMock('Buzz\Message\Form\FormRequest');
+        $response = $this->buildMock('Buzz\Message\Response');
+        $handler = $this->buildMock('AntiMattr\Sears\RequestHandler\AbstractRequestHandler');
+
+        $this->messageFactory->expects($this->once())
+            ->method('createRequest')
+            ->will($this->returnValue($request));
+
+        $this->requestHandlerFactory->expects($this->once())
+            ->method('createRequestHandler')
+            ->with('returnOrders')
+            ->will($this->returnValue($handler));
+
+        $this->messageFactory->expects($this->once())
+            ->method('createResponse')
+            ->will($this->returnValue($response));
+
+        $this->client->returnOrders($collection);
+    }
+
+    public function testUpdateInventory()
+    {
+        $collection = $this->getMock('Doctrine\Common\Collections\Collection');        
+        $request = $this->buildMock('Buzz\Message\Form\FormRequest');
+        $response = $this->buildMock('Buzz\Message\Response');
+        $handler = $this->buildMock('AntiMattr\Sears\RequestHandler\AbstractRequestHandler');
+
+        $this->messageFactory->expects($this->once())
+            ->method('createRequest')
+            ->will($this->returnValue($request));
+
+        $this->requestHandlerFactory->expects($this->once())
+            ->method('createRequestHandler')
+            ->with('updateInventory')
+            ->will($this->returnValue($handler));
+
+        $this->messageFactory->expects($this->once())
+            ->method('createResponse')
+            ->will($this->returnValue($response));
+
+        $this->client->updateInventory($collection);
+    }        
+
+    public function testUpdateProducts()
+    {
+        $collection = $this->getMock('Doctrine\Common\Collections\Collection');        
+        $request = $this->buildMock('Buzz\Message\Form\FormRequest');
+        $response = $this->buildMock('Buzz\Message\Response');
+        $handler = $this->buildMock('AntiMattr\Sears\RequestHandler\AbstractRequestHandler');
+
+        $this->messageFactory->expects($this->once())
+            ->method('createRequest')
+            ->will($this->returnValue($request));
+
+        $this->requestHandlerFactory->expects($this->once())
+            ->method('createRequestHandler')
+            ->with('updateProducts')
+            ->will($this->returnValue($handler));
+
+        $this->messageFactory->expects($this->once())
+            ->method('createResponse')
+            ->will($this->returnValue($response));
+
+        $this->client->updateProducts($collection);
+    }
+
+    public function testUpdateShipments()
+    {
+        $collection = $this->getMock('Doctrine\Common\Collections\Collection');        
+        $request = $this->buildMock('Buzz\Message\Form\FormRequest');
+        $response = $this->buildMock('Buzz\Message\Response');
+        $handler = $this->buildMock('AntiMattr\Sears\RequestHandler\AbstractRequestHandler');
+
+        $this->messageFactory->expects($this->once())
+            ->method('createRequest')
+            ->will($this->returnValue($request));
+
+        $this->requestHandlerFactory->expects($this->once())
+            ->method('createRequestHandler')
+            ->with('updateShipments')
+            ->will($this->returnValue($handler));
+
+        $this->messageFactory->expects($this->once())
+            ->method('createResponse')
+            ->will($this->returnValue($response));
+
+        $this->client->updateShipments($collection);
+    }     
 }
 
 class FakeResponseClientStub extends FakeResponseClient
