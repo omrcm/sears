e5a41d346980ec089f69af82f369ee36b8031b01
carawarner <cwarner@opensky.com>
2014-07-07 13:25:38 -0400

    Explain need for timezone in tests

--- tests/AntiMattr/Tests/AntiMattrTestCase.php
+++ tests/AntiMattr/Tests/AntiMattrTestCase.php
@@ -11,6 +11,12 @@ abstract class AntiMattrTestCase extends \PHPUnit_Framework_TestCase
             ->getMock();
     }
 
+
+    /*
+     * Returns DateTime with timezone set. If timezone is not set, you'll get the following error:
+     * Exception: DateTime::__construct(): It is not safe to rely on the system's timezone settings.
+     * You are *required* to use the date.timezone setting or the date_default_timezone_set() function.
+     */
     protected function newDateTime($time = null)
     {
         $time = $time ? : 'now';
