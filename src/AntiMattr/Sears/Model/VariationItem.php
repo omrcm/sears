4798d0c94e075a5bb24603acf13cd1d92bfc161d
carawarner <cwarner@opensky.com>
2014-07-14 11:14:57 -0400

    Defined Variation Item

--- src/AntiMattr/Sears/Model/VariationItem.php
+++ src/AntiMattr/Sears/Model/VariationItem.php
@@ -29,6 +29,9 @@ class VariationItem implements RequestSerializerInterface
     protected $effectiveStartDate;
 
     /** @var string */
+    protected $id;
+
+    /** @var string */
     protected $image;
 
     /** @var float */
@@ -41,13 +44,151 @@ class VariationItem implements RequestSerializerInterface
     protected $upc;
 
     /**
-     *
+     * @param float $cost
      */
-    protected $variationAttributes;
+    public function setCost($cost)
+    {
+        if (!is_numeric($cost)) {
+            return;
+        }
+
+        $this->cost = (float) $cost;
+    }
+
+    /**
+     * @return float $cost
+     */
+    public function getCost()
+    {
+        return $this->cost;
+    }
+
+    /**
+     * @param string $country
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return string $country
+     */
+    public function getCountry()
+    {
+        return (!isset($this->country)) ? 'US' : $this->country;
+    }
+
+    /**
+     * @param DateTime $startDate
+     */
+    public function setEffectiveStartDate(DateTime $startDate)
+    {
+        $this->effectiveStartDate = $startDate;
+    }
+
+    /**
+     * @return DateTime $effectiveStartDate
+     */
+    public function getEffectiveStartDate()
+    {
+        return $this->effectiveStartDate;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @return string $image
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param float $msrp
+     */
+    public function setMsrp($msrp)
+    {
+        if (!is_numeric($msrp)) {
+            return;
+        }
+
+        $this->msrp = (float) $msrp;
+    }
+
+    /**
+     * @return float $msrp
+     */
+    public function getMsrp()
+    {
+        return $this->msrp;
+    }
+
+    /**
+     * @param string $sellerTags
+     */
+    public function setSellerTags($sellerTags)
+    {
+        $this->sellerTags = $sellerTags;
+    }
+
+    /**
+     * @return array $sellerTags
+     */
+    public function getSellerTags()
+    {
+        return $this->sellerTags;
+    }
+
+    /**
+     * @param string $upc
+     */
+    public function setUpc($upc)
+    {
+        $this->upc = $upc;
+    }
+
+    /**
+     * @return string $upc
+     */
+    public function getUpc()
+    {
+        return $this->upc;
+    }
 
     public function toArray()
     {
         $required = array(
+            'id'        => $this->getId(),
+            'upc'       => $this->getUpc(),
+            'cost'      => $this->getCost(),
+            'msrp'      => $this->getMsrp(),
+            'startDate' => $this->getEffectiveStartDate()->format('Y-m-d'),
+            'image'     => $this->getImage(),
+            'country'   => $this->getCountry(),
         );
 
         // Raise exception if any required parameters are missing
@@ -63,9 +204,30 @@ class VariationItem implements RequestSerializerInterface
             throw new IntegrationException($message);
         }
 
+        // Prepare complex arguments
+        $tags = array();
+        foreach ($this->getSellerTags() as $sellerTag) {
+            $tags[] = array('seller-tag' => $sellerTag);
+        }
+
+        $attributes = array(); //TODO
+
         // Serialize. The order of XML elements must match the sequence laid out in the XSD.
         return array(
-
+            'upc'                   => $required['upc'],
+            'item-prices'           => array(
+                'item-price'        => array(
+                    'cost'          => $required['cost'],
+                    'msrp'          => $required['msrp'],
+                    'effective-start-date' => $required['effective-start-date'],
+                )
+            ),
+            'image-url'             => $required['image'],
+            'variation-attributes'  => $attributes,
+            'country-of-origin'     => array(
+                'country-code'      => $required['country']
+            ),
+            'seller-tags'           => $tags,
         );
     }
 
